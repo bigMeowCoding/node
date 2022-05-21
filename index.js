0d264c709731dda95caeedd8f5c157131bedf6fb
zhouyijun <zhou495248579@hotmail.com>
2022-05-21 15:47:07 +0800

    refactor:prettier

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const http = require('http');
-var mysql      = require('mysql');
+const http = require("http");
+var mysql = require("mysql");
 var connection = mysql.createConnection({
-    host     : '192.168.0.102',
-    user     : 'root',
-    password : '222',
-    database : 'book',
-    port:'3306'
+  host: "192.168.0.102",
+  user: "root",
+  password: "222",
+  database: "book",
+  port: "3306",
 });
 
 connection.connect();
-
-connection.query('SELECT * FROM book;', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].name,results[0].prize);
+connection.query("SELECT * FROM book;", function (error, results, fields) {
+  if (error) throw error;
+  console.log("The solution is: ", results[0].name, results[0].prize);
 });
 // const server = http.createServer((req, res) => {
 //     res.statusCode = 200;
